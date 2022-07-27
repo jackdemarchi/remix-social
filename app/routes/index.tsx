0bc6f11153f31b01983949e1d2cc873d1f1cbb2e
jackdemarchi <giacomodemarchi05@gmail.com>
2022-07-27 16:15:07 +0200

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,78 @@
+import {
+  ActionFunction,
+  json,
+  LoaderFunction,
+  redirect,
+} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { createPost, getPosts } from "~/services/post.server";
+import type { Post } from "~/services/post.server";
+import { Post as PostComponent } from "~/components/Post";
+import { PostForm } from "~/components/PostForm";
+import { CreatePost } from "~/services/validations";
+
+type LoaderData = {
+  posts: Post[];
+};
+
+type ActionData = {
+  error: {
+    formError?: string[];
+    fieldErrors?: {
+      title?: string[];
+      body?: string[];
+    };
+  };
+  fields: {
+    title?: string;
+    body?: string;
+  };
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const rawTitle = form.get("title");
+  const rawBody = form.get("body");
+  const result = CreatePost.safeParse({ title: rawTitle, body: rawBody });
+
+  if (!result.success) {
+    return json(
+      {
+        error: result.error.flatten(),
+        fields: {
+          title: rawTitle,
+          body: rawBody,
+        },
+      },
+      { status: 400 }
+    );
+  }
+  await createPost({
+    title: result.data.title ?? null,
+    body: result.data.body ?? null,
+  });
+
+  return redirect("/");
+};
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = { posts: await getPosts() };
+  return json(data);
+};
+
+export default function Index() {
+  const { posts } = useLoaderData<LoaderData>();
+  return (
+    <div>
+      <h1>Welcome to Remix</h1>
+      <PostForm action="/?index" />
+      <ul>
+        {posts.map((post) => (
+          <li key={post.title}>
+            <PostComponent header={post?.title}>{post?.body}</PostComponent>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
